a5c70643cf12248e10b162cc37b9617415de58a7
Wijnand Boerma <>
2023-07-07 22:37:25 +0200

    no message

--- snippets/test/index.php
+++ snippets/test/index.php
@@ -4,9 +4,10 @@
         <meta charset="utf-8">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Wynand.academy - Code snippets</title>
+        <title>Wynand.academy - Snippets - Icon bounce</title>
+        <link rel="stylesheet" href="<?php echo "http://" . $_SERVER['SERVER_NAME']; ?>/snippets/assets/main.css">
     </head>
     <body>
-    
+        <div class="test"></div>
     </body>
 </html>
