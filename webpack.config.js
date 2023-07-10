6b380596558256458ca8ff8691e8d12f379abd0b
Wijnand Boerma <>
2023-07-10 19:01:07 +0200

    Set-up and drip snippet added

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries"); //Used to delete the automaticaly generated js files
+
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+      '/base/main': './base/js/app.js',
+      '/drop/style': './drop/style.scss',
+      '/viewport-mixin/style': './viewport-mixin/style.scss',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './'),
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s?[c]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(svg)$/i,
+        use: 'url-loader?limit=1024'
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        loader: 'file-loader',
+        options: { outputPath: '/assets/images', publicPath: '../assets/images', useRelativePaths: false}
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        loader: 'file-loader',
+        options: { outputPath: '/fonts', publicPath: '../assets/fonts', useRelativePaths: true }
+      },
+    ],
+  },
+};
