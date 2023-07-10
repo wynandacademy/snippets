c625ec63cbe0c8ca745f423a61a24270851b7abf
Wijnand Boerma <>
2023-07-10 20:36:30 +0200

    no message

---
+++ README.md
@@ -0,0 +1,5 @@
+# Snippets
+Front-end snippets
+
+More information:
+[wynand.academy/snippets](https://wynand.academy/snippets)
---
+++ drip/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Wynand.academy - Snippets</title>
+        <link rel="stylesheet" href="/drip/style.css">
+    </head>
+    <body>
+
+        <!-- Add class 'drip' to the parent-->
+        <main class="container drip">
+
+            <!-- Add up to 20 divs with the class 'drip__drop' -->
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+            <div class="drip__drop"></div>
+
+            <!-- Add class 'drip--from-bottom' if you want to drip from the bottom of the element -->
+            <div class="text drip drip--from-bottom">
+                DRIP
+                <div class="drip__drop"></div>
+                <div class="drip__drop"></div>
+                <div class="drip__drop"></div>
+                <div class="drip__drop"></div>
+                <div class="drip__drop"></div>
+                <div class="drip__drop"></div>
+            </div>
+        </main>
+
+    </body>
+</html>
